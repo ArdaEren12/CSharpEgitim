9999b92a3530f97b9c75976c5397626c513b14aa
ArdaEren12 <148133277+ArdaEren12@users.noreply.github.com>
2024-10-22 00:28:21 +0300

    CSharpEgitim

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdırmaKomutları
+
+            #endregion
+            Console.WriteLine("***** yemek katagorileri *****");
+            Console.WriteLine("");
+            Console.WriteLine("corba");
+            Console.WriteLine("ana yemek");
+            Console.WriteLine("salatalar");
+            Console.WriteLine("icecekler");
+
+            Console.Read();
+            #region Değişkenler
+
+
+
+
+            #endregion
+
+        }
+    }
+}
